5a7e6a96d878a3a329799c076e7288fd148c5d96
ibfmultiplayer <53619140+ibfmultiplayer@users.noreply.github.com>
2019-08-02 22:15:26 +0100

    Added league schedule generator and player/team options

---
+++ tools/options.py
@@ -0,0 +1,63 @@
+import json
+import csv
+import re
+
+''' 
+------------------------------------------
+CHANGE FILENAME AND CURRENT YEAR HERE
+'''
+filename = 'currentExport.json'
+year = 2109
+'''
+------------------------------------------
+'''
+
+# Open json export file
+with open(filename, 'r', encoding='utf-8-sig') as read_file:
+        export = json.load(read_file)
+
+# We only need the player details
+players = export['players']
+
+# We will store contract info here
+contracts = list()
+
+# Add the headings as the first row of the contract list
+headings = ['Name', 'Team', 'Salary', 'Option', 'Option Years']
+contracts.append(headings)
+
+# Make a team code:team name dictionary to use in output file
+teams = { team['tid'] : team['region'] + ' ' + team['name'] for team in export['teams'] }
+
+# Iterate through each player and store their info
+for player in players:
+        info = list()
+        
+        if (player['tid'] >= 0) & (int(player['contract']['exp']) == year) & ('PO' in player['born']['loc'] or 'TO' in player['born']['loc']): # We only want active players who expire this season
+                
+                info.append(player['firstName'] + " " + player['lastName']) # Player Name
+                
+                info.append(teams[player['tid']]) # Player team
+                
+                info.append("$" + str(player['contract']['amount'] / 1000) + "M") # Player Contract
+                
+                # Get contract option status from location field
+                if 'PO' in player['born']['loc']:
+                        info.append('Player')
+                elif 'TO' in player['born']['loc']:
+                        info.append('Team')
+                else:
+                        info.append('None')
+                
+                match = re.search('\d\d-\d\d', player['born']['loc'])
+                if match:
+                        info.append(match.group(0))
+                else:
+                        info.append(year + 1)
+                
+                contracts.append(info) # Add info for this player to our list
+
+# Write contract info to csv
+with open('options.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(contracts)
---
+++ tools/schedule.py
@@ -0,0 +1,72 @@
+import json
+import random
+
+''' 
+------------------------------------------
+CHANGE FILENAME HERE
+'''
+filename = 'currentExport.json'
+'''
+------------------------------------------
+'''
+
+# Open json export file
+with open(filename, 'r', encoding='utf-8-sig') as read_file:
+        export = json.load(read_file)
+        
+# Get the team/division details
+teams = export['teams']
+d1_teams = list()
+d2_teams = list()
+
+for team in teams):
+        if team['cid'] == 0:
+                d1_teams.append(team['tid'])
+        else:
+                d2_teams.append(team['tid'])
+
+# Randomise team order
+d1_teams = random.sample(d1_teams, len(d1_teams))
+d2_teams = random.sample(d2_teams, len(d2_teams))
+
+def makeSchedule(d1_teams, d2_teams):
+        # Makes a league schedule following the algorithm outlined at https://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm
+        
+        n = len(d1_teams) # assume d1 & d2 have equal size
+        mid = int(n/2) # number of games per division per day
+        schedule = list() # will store games here
+        order = [0, n-1] # used to reorder the teams list each matchday
+
+        for i in range(1, n-1):
+                order.append(i)
+        
+        for i in range((n-1) * mid):
+                index = i % mid
+                if (((i+1) // mid) % 2) == 0: # alternates home/away teams to avoid long stretches at home/away
+                        schedule.append({'homeTid':d1_teams[index], 'awayTid':d1_teams[-(index+1)]}) # add a match to the schedule
+                        schedule.append({'homeTid':d2_teams[index], 'awayTid':d2_teams[-(index+1)]})
+                else:
+                        schedule.append({'homeTid':d1_teams[-(index+1)], 'awayTid':d1_teams[index]})
+                        schedule.append({'homeTid':d2_teams[-(index+1)], 'awayTid':d2_teams[index]})
+                if (i+1) % mid == 0: # reorder the teams lists at the end of a matchday
+                        d1_teams = [d1_teams[j] for j in order]
+                        d2_teams = [d2_teams[j] for j in order]
+        
+        return schedule
+
+schedule = makeSchedule(d1_teams, d2_teams)
+
+rev_schedule = list()
+# Create the away legs
+for match in schedule:
+        rev_schedule.append({ 'homeTid':match['awayTid'], 'awayTid':match['homeTid'] })
+        
+# IBF has 4 rounds of fixtures
+schedule = (schedule + rev_schedule) * 2
+
+# Replace the existing schedule with our own
+export['schedule'] = schedule
+
+# Write to a bbgm league file
+with open('currentExport.json', 'w') as outfile:
+        json.dump(export, outfile)
