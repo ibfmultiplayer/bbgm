25b9afcc49d13a656c189198bcee08f9e7a66254
ibfmultiplayer <53619140+ibfmultiplayer@users.noreply.github.com>
2020-09-18 12:56:06 +0100

    Altered to work with new voting sheet

--- tools/fa.py
+++ tools/fa.py
@@ -37,7 +37,7 @@ def main():
 
         for row in rows[1:]:
                 x = { key : row[keys.index(key)] for key in keys }
-                if x['Wave Raw Output'] != "": # anyone who didn't sign has empty string for this field
+                if x['Sign'] =="Y": # anyone who didn't sign has empty string for this field
                         signings.append(x) # store the players who did sign
         
         if 'preseason' in export['meta']['phaseText']:
@@ -45,43 +45,17 @@ def main():
         else:
                 year = int(export['meta']['phaseText'][:export['meta']['phaseText'].index(' ')])
                         
-        for signing in signings: # convert summary column Yrs/$M into salary amount, contract years & any options
+        for signing in signings:
                 
-                # Anything between '/' and 'M' is our annual salary
-                signing['amount'] = int(float(signing['Yrs/$M'][signing['Yrs/$M'].index("/") + 1:signing['Yrs/$M'].index("M")]) * 1000)
+                signing['amount'] = int(float(signing['Salary'][signing['Salary'].index("$") + 2:signing['Salary'].index("M")]) * 1000)
                 
-                # + means there is a player/team option
-                if '+' in signing['Yrs/$M']:
-                        signing['exp'] = int(signing['Yrs/$M'][:signing['Yrs/$M'].index("+")]) + year
-                else:
-                        signing['exp'] = int(signing['Yrs/$M'][:signing['Yrs/$M'].index("/")]) + year
+                signing['exp'] = int(signing['Years']) + year
                 
                 # Any options/NTCs are stored in the player 'loc' field
                 signing['locModifier'] = ''
-                multi_option = re.search('\d((PO)|(TO))', signing['Yrs/$M']) # A number before PO/TO means it is for multiple years
-                if multi_option:
-                        option_start = signing['exp'] + 1 # options start the year after expiry
-                        option_end = int(multi_option.group(0)[0]) + signing['exp'] # get number of option years and add it to expiry year
-                        option_type = multi_option.group(0)[1:] # get option type (PO/TO) from matched pattern
-                        # add option info to our modifier
-                        signing['locModifier'] += ' - ' + str(option_start)[2:] + '-' + str(option_end)[2:] + ' ' + option_type
-                        
-                elif 'PO' in signing['Yrs/$M']: # as above but for single year options
-                        option_start = signing['exp'] + 1
-                        option_type = 'PO'
-                        
-                        signing['locModifier'] += ' - ' + str(option_start)[2:] + ' ' + option_type
-                        
-                elif 'TO' in signing['Yrs/$M']: # as above but for single year options
-                        option_start = signing['exp'] + 1
-                        option_type = 'TO'
-                        
-                        signing['locModifier'] += ' - ' + str(option_start)[2:] + ' ' + option_type
                 
-                if 'NTC' in signing['Yrs/$M']:
-                        signing['locModifier'] += ' NTC'
-                if 'NLC' in signing['Yrs/$M']:
-                        signing['locModifier'] += ' NLC'
+                if signing['Option'] != 'None':
+                        signing['locModifier'] = ' - ' + signing['Option']
 
         # Make a team name:tid dictionary to use in output file
         teams = { team['region'] + ' ' + team['name'] : team['tid'] for team in export['teams'] }
@@ -90,13 +64,24 @@ def main():
                 
                 for signing in signings:
                         
-                        if player['firstName'] + ' ' + player['lastName'] == signing['Player name']: # check if the player is in our list of signings
+                        if player['firstName'] + ' ' + player['lastName'] == signing['Player']: # check if the player is in our list of signings
                                 player['tid'] = teams[signing['Team']]
                                 player['contract']['amount'] = signing['amount']
                                 player['contract']['exp'] = signing['exp']
                                 if '-' in player['born']['loc']: # loc has a '-' iff a player had options stored from a previous contract
                                         player['born']['loc'] = player['born']['loc'][:player['born']['loc'].index('-')] # remove previous options
                                 player['born']['loc'] += signing['locModifier'] # add new options info
+                                
+                                if signing['Years'] == '1':
+                                        if signing['Option'] == 'None':
+                                                print(signing['Player'] + ' signs a ' + signing['Years'] + ' year, ' + signing['Salary'] + ' contract with @' + signing['Team'])
+                                        else:
+                                                print(signing['Player'] + ' signs a ' + signing['Years'] + ' year + ' + signing['Option'] + ', ' + signing['Salary'] + ' contract with @' + signing['Team'])
+                                else:                             
+                                        if signing['Option'] == 'None':
+                                                print(signing['Player'] + ' signs a ' + signing['Years'] + ' years, ' + signing['Salary'] + ' contract with @' + signing['Team'])
+                                        else:
+                                                print(signing['Player'] + ' signs a ' + signing['Years'] + ' years + ' + signing['Option'] + ', ' + signing['Salary'] + ' contract with @' + signing['Team'])
                                 
         # write new league file
         with open('ibfExport.json', 'w') as outfile:
